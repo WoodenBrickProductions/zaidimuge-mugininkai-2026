debccf5b5f0b6ffa5442772e8486c38c0acd44d3
CheesyRobot <ievalt2020@gmail.com>
2026-03-14 18:47:27 +0200

    Setup and Database test
    AppActivity added to manifest file
    Changed back some build.gradle lines to be consistent with main branch (some things were different because rokas's branch is on an older Android Studio version)

--- app/src/main/java/com/example/universityjava/MainActivity.java
+++ app/src/main/java/com/example/universityjava/MainActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppDatabase db;
     Button _button;
 
     @Override
@@ -16,10 +20,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = AppActivity.getDatabase();
         _button = (Button) findViewById(R.id.button);
         _button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*User user = new User();
+                user.setName("Cheesy");
+                user.setEmail("CheesyMail");
+                user.setPassword("Password");
+                db.userDAO().insert(user);
+                List<User> userList = db.userDAO().getAllUsers();
+                User u = userList.get(0);
+                Toast.makeText(getApplicationContext(), u.getName() + u.getEmail(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Registered successfully", Toast.LENGTH_SHORT).show();
+                */
                 System.out.println("OnClick");
                 Intent intent = new Intent(getBaseContext(), SecondWindow.class);
                 intent.putExtra("Data", "Hello World");
