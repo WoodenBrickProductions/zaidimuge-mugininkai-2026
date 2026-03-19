373334258f85220cc9d923a3e12e2e327ee2f8e8
Rocikm <rokassimkunass@gmail.com>
2026-03-19 14:12:01 +0200

    Add "back" functionality to fragments

--- app/src/main/java/com/example/universityjava/MainActivity.java
+++ app/src/main/java/com/example/universityjava/MainActivity.java
@@ -79,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frameLayoutMain, fragment);
+        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
 
