23e12f518fbabd2a419345714ea51466ce518c9c
Tautvydas (Wooden_Brick) <tautvisceponis@gmail.com>
2026-04-30 14:42:03 +0300

    Loading images works

--- app/src/main/java/com/example/universityjava/AddListingFragment.java
+++ app/src/main/java/com/example/universityjava/AddListingFragment.java
@@ -95,17 +95,14 @@ public class AddListingFragment extends Fragment {
                     game.setId(AppActivity.getDatabase().gameDAO().insert(game));
                 }
 
-                SharedPreferences prefs = getContext().getSharedPreferences("my_prefs", Context.MODE_PRIVATE);
-
                 var listing = new Listing();
                 listing.setFk_gameid(game.getId());
                 listing.setFk_seller(AppActivity.getCurrentUserID());
                 listing.setPrice(Double.parseDouble(addListingPrice.getText().toString()));
-                final int DUMMY_ID = 100000; // TODO(Woody): implement
+                final int DUMMY_ID = 1; // TODO(Woody): implement
                 listing.setFk_platform(DUMMY_ID);
 
                 listing.setId(AppActivity.getDatabase().listingDAO().insert(listing));
-                var fragment = ListingPageFragment.newInstance(listing.getId(), "");
                 ((MainActivity)getActivity()).replaceFragment(new HomeFragment());
             }
         });
