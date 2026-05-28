aa463210becd28d19b52def7ac4ed27d495628f3
CheesyRobot <ievalt2020@gmail.com>
2026-05-28 13:43:44 +0300

    imlp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation("androidx.activity:activity:1.13.0")
     implementation("com.google.code.gson:gson:2.14.0")
     implementation("com.google.android.gms:play-services-maps:20.0.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
